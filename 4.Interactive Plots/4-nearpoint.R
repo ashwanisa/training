4917a14a555e28f1a0a125b90e0bd38d59b8c4d0
ashwanisa <ashwani.saxena04@gmail.com>
2017-07-26 11:05:34 +0530

    Add files via upload

--- 4.Interactive Plots/4-nearpoint.R
+++ 4.Interactive Plots/4-nearpoint.R
@@ -1,3 +1,4 @@
+library(shiny)
 ui <- basicPage(
   plotOutput("plot1", click = "plot_click"),
   verbatimTextOutput("info")
@@ -10,7 +11,7 @@ server <- function(input, output) {
   
   output$info <- renderPrint({
     # With base graphics, need to tell it what the x and y variables are.
-    nearPoints(mtcars, input$plot_click, x= wt,y = mpg, threshold = 100,maxpoints = 10)
+    nearPoints(mtcars, input$plot_click,xvar = "wt", yvar="mpg", threshold = 25,maxpoints = 2)
     # nearPoints() also works with hover and dblclick events
   })
 }
@@ -39,7 +40,7 @@ server <- function(input, output) {
     # threshold: set max distance, in pixels
     # maxpoints: maximum number of rows to return
     # addDist: add column with distance, in pixels
-    nearPoints(mtcars, input$plot_click, threshold = 100, maxpoints = 10,
+    nearPoints(mtcars, input$plot_click, threshold = 10, maxpoints = 10,
                addDist = TRUE)
   })
 }
